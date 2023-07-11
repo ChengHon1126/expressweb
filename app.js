278542218b63b796cb2a52d91d5cf42e7e5a24f5
Cheng <lkmd555@gmail.com>
2023-07-11 18:39:27 +0800

    session/cookie and redis

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const path = require("path");
 const hbs = require("hbs"); //記得 npm install hbs;
@@ -17,11 +18,15 @@ const validator = require("./utils/validator.js");
 // npm install redis@v3 connect-redis
 
 // 追加 redis 套件 （ Node.js 使用 ）
-// const redis = require("redis");       
-// const redisClient = redis.createClient();  //產生 redisClient 的連線實例 (Instance)
 
+const RedisStore = require("connect-redis").default;
+const redis = require("redis");
+const redisClient = redis.createClient(); 
+let redisStore = new RedisStore({
+    client: redisClient,
+  })
 // 追加 connect-redis 套件 （ 專門為 express 設計的對接套件 ）
-// const redisStore = require("connect-redis").default({session});
+
 /////////////////////////
 app.engine("html", hbs.__express);
 app.set("views", path.join(__dirname, "application","views"))
@@ -37,12 +42,12 @@ app.use(bodyParser.urlencoded({
 // 處理 session 資料的 Middleware
 // 後面才可以用 req.session 做資料存取
 app.use(session({
-    // store : new redisStore({ client : redisClient}),  // session 存放資料的地方
+    store : redisStore,  // session 存放資料的地方
     secret : "abcd1234" ,  // session 資料加密使用
     resave : true,          // 不論修改 , 是否要回存到 store 上
     saveUninitialized : false, // 初始化的 session , 是否要存到 store 上
     name   : "_ntust_tutorial_id",  // cookie 的 key 值
-    ttl    : 24*60*60*1             // session 資料有效時間 (以 s 為單位)
+    expires    : 24*60*60*1             // session 資料有效時間 (以 s 為單位)
 }));
 
 app.use((req,res,next)=>{
@@ -95,3 +100,4 @@ app.get("/about/us",(req,res)=>{
 app.listen(portNum, ()=>{
     console.log('Server is runnung at http://localhost:'+portNum);
 })
+
